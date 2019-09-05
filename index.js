5d5a1a46c06cea191cf924f82821433a3b37a855
May Ng <mngmay@gmail.com>
2019-09-05 09:46:27 -0700

    push

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
+require("dotenv").config();
+console.log("environment", process.env.NODE_ENV);
 const port = 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
