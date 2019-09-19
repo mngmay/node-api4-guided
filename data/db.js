e67c5f80cb702f007a601b39c6fb2722919e0d42
May Ng <mngmay@gmail.com>
2019-09-19 11:19:35 -0700

    test

--- data/db.js
+++ data/db.js
@@ -1,6 +1,7 @@
-const knex = require('knex');
-​
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
-​
+const knex = require("knex");
+const secrets = require("../config/secrets.js");
+
+const environment = secrets.environment || "development";
+const config = require("../knexfile.js")[environment];
+
 module.exports = knex(config);
